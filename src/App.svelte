39c0dca2c43ee86038eaa97732f1bf2e80e6887f
MathildeBillardMarceau <mathildebillard@noos.fr>
2025-07-08 20:21:56 +0200

    affichage 1 msg IA

--- src/App.svelte
+++ src/App.svelte
@@ -1,46 +1,87 @@
 <script>
+// import {onMount} from 'svelte';
 
+let chat = $state ([]);
+let currentChats = $state ("");
+let chatServers = $state ("");
+
+async function handleSubmit (event) {
+    event.preventDefault();
+    const response = await fetch ("https://api.mistral.ai/v1/chat/completions",  {
+        method: "POST",
+        headers: {
+        "Authorization": "Bearer BraU6oMJjYOwhzFPu6elA0YdC1qQ78Z8",
+        "content-type" : "application/json"
+                },
+        body: JSON.stringify(
+            {
+                model: "mistral-small-latest",
+                messages: [
+                    {
+                        role: "user",
+                        content: currentChats,
+                    }
+                ]
+            }
+        )
+        }
+    )
+chatServers = await response.json();
+console.log(chatServers);
+chat = chatServers.choices[0].message.content;
+console.log(chat);
+document.querySelector('.messages').textContent = chat;
+}
+
+
+
+// onMount(handleSubmit)
 </script>
 
-
-<body class="homepage">
+<div class="homepage__container" >
     
-    <div class="homepage__container" >
+    <header class="homepage__container__header">
+        <div class="homepage__container_header--logo">
+            <img src="/elements/logo.png" alt="inscription MANCHASK suivi du chat Manchas" class="logo">
+        </div>
+        <p class="homepage__container__header--questions">Tu te poses des questions ? <br> Manchas te réponds.</p>
+    </header> 
+    
+    <main class="zonedesaisie">
         
-        <header class="homepage__container__header">
-            <div class="homepage__container_header--logo">
-                <img src="/elements/logo.png" alt="inscription MANCHASK suivi du chat Manchas" class="logo">
-            </div>
-            <p class="homepage__container__header--questions">Tu te poses des questions ? <br> Manchas te réponds.</p>
-        </header> 
-        
-        <main class="zonedesaisie">
+        <div class="homepage__container__zonedesaisie__inputcontainer" >
+            <div class="messages">
 
-            <div class="homepage__container__zonedesaisie__inputcontainer" >
-                <!-- <div class="onlyContainer"> -->
-
-                    <input type="text" class="homepage__container__zonedesaisie__inputcontainer--input" placeholder=" Pose ta question à Manchas...">
-                    <button type="submit" aria-label="Envoyer" class="homepage__container__zonedesaisie__inputcontainer__icone--button">  
-                        <img src="/elements/icons8-search-50.png" alt="Envoyer" style="width: 42px; height: 42px;">
-                    </button>
-
-                <!-- </div> -->
-            </div>
-        
+        </div>
+            <form onsubmit={handleSubmit} class="homepage__container__zonedesaisie__inputcontainer__form"> 
+                
+                <input bind:value={currentChats} type="text" class="homepage__container__zonedesaisie__inputcontainer__form--input" placeholder=" Pose ta question à Manchas...">
+                <button type="submit" aria-label="Envoyer" class="homepage__container__zonedesaisie__inputcontainer__icone--button">  
+                    <img src="/elements/icons8-search-50.png" alt="Envoyer" style="width: 42px; height: 42px;">
+                </button>
+                
+            </form>
+                        
+        </div>
         </main>
         
         <footer class="historique">
-            <img src="/elements/historique.png" alt="" class="homepage__container__footer">
+            <div class="homepage__historique__dropdown">
+                <button class="homepage__historique__dropdown--button">
+                    <img src="/elements/historique.png" alt="" class="homepage__container__footer">
+                </button>
+                <div class="homepage__historique__dropdown--child">
+                    <a href="http://localhost:5173/" target="_blank"> Pourquoi le ciel est bleu ?</a>
+                    <a href="http://localhost:5173/" target="_blank"> Combien de croquettes ?</a>
+                </div>
+            </div>
         </footer>
     </div>
     
-</body>
+
 
 <style>
 
-/* .onlyContainer {
-    display:inline;
-} */
 
 .homepage__container__zonedesaisie__inputcontainer {
     display: flex;
@@ -56,19 +97,27 @@
 
 }
 
-.homepage__container__zonedesaisie__inputcontainer--input {
+.homepage__container__zonedesaisie__inputcontainer__form {
     height: 2.5rem;
     border: 1px solid black;
+    
     border-radius: 35px;
     width:initial;
-    /* display: grid; */
-    box-shadow: 4px 5px rgba(0, 0, 0, 0.25);
+    box-shadow: 0 8px 16px rgba(0,0,0,0.2);
     margin: 15px;
-    /* position: absolute; */
+
 }
 
+.homepage__container__zonedesaisie__inputcontainer__form--input {
+    height: 2.3rem;
+    box-shadow: 0 8px 16px rgba(0,0,0,0.2);
+    border-radius: 35px;
+    border: none;
+    justify-content: center;
+}
+
+
 .homepage__container__zonedesaisie__inputcontainer__icone--button {
-    /* position:sticky; */
     border: none;
     background: none;
     cursor: pointer;
@@ -80,6 +129,47 @@
 .homepage__container__header--questions {
     text-align: end;
     margin: 3% 5%;
+}
+
+.historique {
+    display: flex;
+    justify-content: center;
+    position: relative;
+
+}
+
+.homepage__historique__dropdown--button {
+    border: none;
+    background: none;
+    cursor: pointer;
+} 
+
+
+.homepage__historique__dropdown--child {
+    display: none;
+    justify-content: center;
+    background: white;
+    min-width: 200px;
+    box-shadow: 0 8px 16px rgba(0,0,0,0.2);
+    z-index: 1;
+    border-radius: 8px;
+    margin-top: 10px;
+}
+
+
+.homepage__historique__dropdown--child a {
+    color: black;
+    padding: 10px;
+    text-decoration: none;
+    display: block;
+}
+
+.homepage__historique__dropdown--child a:hover {
+    background-color: #E3E0DE;
+}
+
+.historique:hover .homepage__historique__dropdown--child {
+    display: block;
 }
 
 </style>
